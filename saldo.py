69b80b9ca2b363f2c54788f4beb3a444343eec5c
Ruan08042008 <ruanpablogarcia43@gmail.com>
2026-09-02 12:51:04 -0300

    Add files via upload

---
+++ saldo.py
@@ -0,0 +1,7 @@
+saldo = float(input("Digite seu saldo: "))
+compra = float(input("Digite o valor da sua compra: "))
+
+if saldo >= compra:
+    print("Saldo Insuficiente")
+else:
+    print("Saldo Insuficiente")
