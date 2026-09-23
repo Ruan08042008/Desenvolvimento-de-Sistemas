4cd68e5c84d1ffe13d7cdef310fcd6fd4528534e
Ruan08042008 <ruanpablogarcia43@gmail.com>
2026-09-23 12:47:58 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+temperatura = float(input("digite a temperatura: "))
+
+if temperatura >=35:
+    print("muito quente")
+elif temperatura >=30:
+    print("quente")
+elif temperatura >=18:
+    print("frio")
+else:
+    print("muito frio")
---
+++ zero.py
@@ -0,0 +1,9 @@
+numero = int(input("digite o numero: "))
+
+if numero >0:
+    print("positivo")
+elif numero <0:
+    print("negativo")
+else:
+    print("zero")
+
