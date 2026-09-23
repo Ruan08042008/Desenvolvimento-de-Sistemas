9f61ecb7e2bc6159d26029b37220500df9e70b93
Ruan08042008 <ruanpablogarcia43@gmail.com>
2026-09-23 13:04:40 -0300

    Add files via upload

---
+++ opcao.py
@@ -0,0 +1,13 @@
+opcao = int(input("Escolha uma opção: "))
+if opcao ==1:
+    print("Urologista")
+elif opcao ==2:
+    print("Medico")
+elif opcao ==3:
+    print("Escravo")
+elif opcao ==4:
+    print("Gogo Boy")
+elif opcao ==5:
+    print("Medigo")
+else:
+    print("opção invalida")
